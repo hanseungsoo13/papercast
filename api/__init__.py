4272c46e0d5d8a967d000c04f13be03d6167b9eb
hanseungsoo13 <hanseungsoo63@gmail.com>
2025-11-01 17:50:42 +0900

    chore: refactor API structure and add Firebase deployment

--- api/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-PaperCast API Package
-
-FastAPI 기반 웹 API 레이어
-기존 src/ 파이프라인에서 생성된 JSON 데이터를 읽기 전용으로 제공
-"""
-
-__version__ = "1.0.0"
-__author__ = "hanseungsoo13"
--- api/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-API Routes Package
-
-FastAPI 라우터들을 포함하는 패키지
-"""
